e84b5f9c2b0b660c53b6ff8ba38ca1c18c673bfb
Marco <msmo1960@gmail.com>
2021-05-13 16:31:29 -0500

    first commit of work

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,39 @@
+
+
+const inquirer = require("inquirer");
+//const fs = require("fs");
+//const gerateMarkdown = require("./utils/generateMarkdown");
+//const api = require("./utils/api");
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: "input",
+    name: "ProjectTitle",
+    message: "What is the name of the Project Title?"
+}
+
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    try {
+    const answers = promptUser();
+}catch(err) {
+    console.log(err);
+}
+};
+
+function promptUser(){
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
